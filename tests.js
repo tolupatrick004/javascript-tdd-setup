e452c7eefdc67ce3f1bfbbf2fbda91e5dbbfc048
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2016-10-15 17:06:52 +0100

    more specs

--- tests.js
+++ tests.js
@@ -50,6 +50,61 @@ describe("Prime Number Validity Tests", function(){
         var expected = false;
         assert.equal(test, expected,"Invalid Input, user entered an array.");
     })
+
+    it("Checks if the value is an empty array", function(){
+        var number = [];
+        var test = check_prime_number(number);
+        var expected = false;
+        assert(test == expected);
+    })
+
+    it("Check if the value entered is zero", function(){
+        var number = 0;
+        var test = check_prime_number(number);
+        var expected = false;
+        assert.equal(test, expected);
+    })
+
+    it("Checks if value entered is a character", function(){
+        var letter = A
+        letter = letter.toLowerCase();
+        var expected = a;
+        assert.equal(test, expected,"Invalid input, character token not allowed");
+    })
+
+    it("Checks if the value entered is a special character", function(){
+        var special_chars = ["!","£","$","%","^","&","*","~","<",">","?","@"];
+        var test = [];
+        var expected = false;
+        for(var i =0; i < special_chars.length; i++){
+            var number = special_chars[i];
+            test[i] = check_prime_number(number);
+        }
+        for (var i = test.length - 1; i >= 0; i--) {
+            assert.equal(test[i],expected);
+        }
+    })
+
+    it("Checks if the value is very large", function(){
+        var number = 1000;
+        var test = check_prime_number(number);
+        var expected = false;
+        assert.equal(test, expected,"Exceeds the maximum value of 500");
+    })
+    
+    it ("Checks if the value entered is very small", function(){
+        var number =1;
+        var test = check_prime_number(number);
+        var expected = false;
+        assert.equal(test, expected);
+    })
+
+    it("Checks if the value entered is a float", function(){
+        var number = 10.5;
+        var test = check_prime_number(number);
+        var expected = false;
+        assert.equal(test,expected,"Incorrect input, number has floating values");
+    })
 })
 
 
